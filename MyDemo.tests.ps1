8bbd13573716194b8d3c23dcfcde822f702bafd4
Simon Wåhlin <simon.wahlin@gmail.com>
2015-10-08 07:44:07 +0200

    added first test

---
+++ MyDemo.tests.ps1
@@ -0,0 +1,13 @@
+﻿Describe 'MyDemo testing' {
+    Context DemoTests {
+        It 'throws when given an error' {
+            {throw} | Should throw
+        }
+        It 'Returns true' {
+            $true | Should Be $true
+        }
+        It 'Fails on thirt test' {
+            $true | Should Be $true
+        }
+    }
+}
